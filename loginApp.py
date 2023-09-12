5f253b91ecdd6712098981c4ea6749156441aa84
Nohan Gustavo <nohan.gnb@gmail.com>
2023-09-11 21:10:19 -0300

    pag inicial
    Começo da criação da pagina inicial e suas funções

--- loginApp.py
+++ loginApp.py
@@ -5,23 +5,21 @@ from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
+from kivymd.uix.boxlayout import BoxLayout
+from kivymd.uix.label import MDLabel
 
 class LoginApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "DeepPurple"
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.accent_palette = "Teal"
-        self.root.current = 'login'
-        
-        self.dialog = MDDialog(text='Insira um nome e senha.', buttons =[
-    MDFlatButton(text='Ok!', on_release=self.dialog.dismiss)        
-])
-        
+        self.root.current = 'logado'
+    
         return Builder.load_file('login.kv')
     
     def mostrar_erro(self):
+        self.root.get_screen('login').ids.errorLabel.text = "Informe os dados corretamente."
 
-        self.root.get_screen('login').add_widget(self.dialog)
 
     # Método login
 
@@ -35,6 +33,14 @@ class LoginApp(MDApp):
             
             print("Por favor, insira nome de usuário e senha.")
 
+    def concluir(self):
+        tarefaLabel = self.root.get_screen('logado').ids.tarefaLabel
+        tarefaLabel.font_style = "Overline"
+    
+    def addTarefa(self):
+        tarefa = MDLabel(text="nova tarefa")
+        self.root.get_screen('logado').ids.telaPrincipal.add_widget(tarefa)
+    
 
 
 LoginApp().run()
